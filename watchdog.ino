828bac08107ea331c754d4d4de96033c7aad2fc2
lokomoti <40903296+lokomoti@users.noreply.github.com>
2021-03-13 21:24:40 +0100

    Add files via upload

---
+++ watchdog.ino
@@ -0,0 +1,73 @@
+const int rst_pin = 13;
+
+int time_out = 0;
+int counter = 0;
+int incomingByte;
+
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("WatchDog v.1");
+  Serial.println("Send some of following options to start the counter:");
+  Serial.println("1 - 60s");
+  Serial.println("2 - 120s");
+  Serial.println("3 - 180s");
+  Serial.println("Reset the counter by sending R character");
+  
+
+  pinMode(rst_pin, OUTPUT);
+  digitalWrite(rst_pin, LOW);
+}
+
+void loop() {
+  handleSerial();
+  if (time_out != 0) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      counter ++;
+      if (counter >= time_out) {
+        reboot();
+      }
+      previousMillis = currentMillis;
+      Serial.println(time_out - counter);
+    }
+  }
+}
+
+void handleSerial() {
+  while (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    switch (incomingByte) {
+      case 'R':
+        counter = 0;
+        break;
+
+      case '1':
+        time_out = 60;
+        break;
+
+      case '2':
+        time_out = 120;
+        break;
+
+      case '3':
+        time_out = 180;
+        break;
+
+      case '0':
+        time_out = 10;
+        break;
+    }
+  }
+}
+
+void reboot() {
+  digitalWrite(rst_pin, HIGH);
+  delay(500);
+  digitalWrite(rst_pin, LOW);
+  time_out = 0;
+  counter = 0;
+}
---
+++ watchdog.py
@@ -0,0 +1,34 @@
+# Imports
+import serial
+import time
+import os.path
+import json
+
+# Read config
+with open(os.path.dirname(__file__) + "/config.json") as f:
+    cfg = json.load(f)
+
+# Assign variables from the config file
+port = cfg["port"]
+time_out = str(cfg["timeOut"])
+interval = cfg["interval"]
+
+# Start serial
+ser = serial.Serial(port, 9600)
+
+# Wait for serial to initiate
+time.sleep(3)
+
+# Set timeout value and start watchdog
+ser.write(time_out.encode())
+# ser.write(b"0")
+
+# Start infinite loop
+try:
+    while True:
+        ser.write(b"R")
+        time.sleep(interval)
+except:
+    pass
+finally:
+    ser.close()
